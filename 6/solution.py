8e0d90a19906a9bdd5d8566a892b1012327accd8
Travis Hesketh <travis.hesketh@airelogic.com>
2021-12-06 16:05:48 +0000

    Updated comment, replaced pop with __getitem__.

--- 6/solution.py
+++ 6/solution.py
@@ -16,7 +16,7 @@ LanternFish = int
 
 
 def parse_input(path: PathLike) -> Sequence[LanternFish]:
-    """Read in the sequence of lanternfish counts from a file."""
+    """Read in a sequence of lanternfish from a file."""
     with open(path, "r", encoding="utf-8") as file:
         return list(map(int, next(file).rstrip().split(",")))
 
@@ -26,7 +26,7 @@ def count_lanternfish_after(lanternfish: Iterable[LanternFish], n_days: int) -> 
     fish_counter = Counter(lanternfish)
 
     for _ in range(n_days):
-        n_spawned = fish_counter.pop(0, 0)
+        n_spawned = fish_counter[0]
 
         for days_til_spawn in range(1, 9):
             fish_counter[days_til_spawn - 1] = fish_counter[days_til_spawn]
